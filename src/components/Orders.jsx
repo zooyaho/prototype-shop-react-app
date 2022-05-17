20a00ceab4e8afaf6a5bd8d2c3b82e90f07c95dc
zooyaho <jiwoo1268@naver.com>
2022-05-17 11:13:10 +0900

    default UI setting complete

---
+++ src/components/Orders.jsx
@@ -0,0 +1,11 @@
+export default function Orders(){
+  return(
+    <aside>
+      <div className="empty">
+        <div className="title">You don't have any orders</div>
+        <div className="subtitle">Click on a + to add an order</div>
+      </div>
+      <div></div>
+    </aside>
+  );
+}
