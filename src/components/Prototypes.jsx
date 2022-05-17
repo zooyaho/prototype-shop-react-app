5c0a5c9c4f09912d93d8beb5e70493726edda467
zooyaho <jiwoo1268@naver.com>
2022-05-17 11:44:09 +0900

    상품 리스트 UI 완료

--- src/components/Prototypes.jsx
+++ src/components/Prototypes.jsx
@@ -135,7 +135,42 @@ const prototypes = [
 export default function Prototypes(){  
   return(
     <main>
-      <div className="prototypes">상품 리스트</div>
+      <div className="prototypes">
+        {prototypes.map((prototype)=> {
+          const {id, thumbnail, title, price, desc, pieUrl} = prototype;
+          return(
+            <div className="prototype" key={id}>
+              <a href={pieUrl} target="_BLANK">
+                <div
+                  style={{
+                    padding: "25px 0 33px 0"
+                  }}>
+                  <video
+                    autoPlay
+                    loop
+                    playsInline
+                    className="prototype__atwork prototype__edit"
+                    src={thumbnail}
+                    style={{
+                      objectFit: "contain"
+                    }}
+                  />
+                </div>
+              </a>
+              <div className="prototype__body">
+                <div className="prototype__title">
+                  <div className="btn btn--primary float--right">
+                    <i className="icon icon--plus"></i>
+                  </div>
+                  {title}
+                </div>
+                <p className="prototype__price">$  {price}</p>
+                <p className="prototype__desc">{desc}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
