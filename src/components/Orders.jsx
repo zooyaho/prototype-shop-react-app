eefb3abe793685e5bea13cfa9f466227df6ab73c
zooyaho <jiwoo1268@naver.com>
2022-05-17 13:27:33 +0900

    상품 추가 시 order영역에 해당 상품 추가

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,8 +1,9 @@
 import useOrders from "../hooks/useOrders";
+import usePrototype from "../hooks/usePrototypes";
 
 export default function Orders(){
   const orders = useOrders();
-  console.log(orders);
+  const prototypes = usePrototype();
 
   if(orders.length == 0) {
     // order에 아무것도 추가가 되지 않았을 경우
@@ -19,7 +20,29 @@ export default function Orders(){
     // order에 추가가 되었을 경우
     return(
       <aside>
-        orders
+        <div className="body">
+          {orders.map(order=> {
+            const {id} = order;
+            // 추가하는 prototype의 id를 이용하여 thumbnail찾음.
+            const prototype = prototypes.find(p=> p.id === id)
+            return (
+              <div className="item" key={id}>
+                <div className="img">
+                  <video src={prototype.thumbnail}></video>
+                </div>
+                <div className="content">
+                  <p className="title">
+                    {prototype.title} X {order.quantity}
+                  </p>
+                </div>
+                <div className="action">
+                  <p className="price">$ {prototype.price * order.quantity}</p>
+                  <button className="btn btn--link"><i className="icon icon--cross"></i></button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </aside>
     );
   }
